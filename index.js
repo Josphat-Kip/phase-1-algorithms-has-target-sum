790eafb84bfcd7f6d834da2e0d6ac1e07508b537
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-03-31 22:39:11 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-  let nums = {};
-  for (let i = 0; i < array.length; i++) {
+  let nums ={}
+  for (let i=0; i<array.length; i++) {
     let num = array[i];
-    if (target - num in nums) {
+    if(target-num in nums){
       return true;
     }
-    nums[array[i]] = i;
+    nums[array[i]]=i;
   }
   return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(N) wher N is the total number of elements in te given array
+  time complexity we get it by taking 0(n)
+  space complexity is gotten by 0(1) 
 */
-O(n);
+
 /* 
   Add your pseudocode here
+*/
 
 /*
   Add written explanation of your solution here
+  we assign a variable to an empty object
+  do a for loop
+  we search for y 
 */
 
 // You can run `node index.js` to view these console logs
